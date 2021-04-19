863d106e527377ee4eb536f5597a9e70b4b563b1
zafna <73626497+zafna@users.noreply.github.com>
2021-04-19 14:16:26 +0400

    Add files via upload

---
+++ js/Block.js
@@ -0,0 +1,38 @@
+class Block {
+  constructor(x,y,width,height) {
+    var options = {
+      'restitution':1,
+      'friction':0.1
+       // isStatic: true
+    }
+    this.visiblity=225;
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    this.image=loadImage("images/block.png");
+    World.add(world, this.body);
+  }
+  score(){
+    if(this.visibility<0 && this.visibility >-105){
+      score++;
+    }
+  }
+  display(){
+
+    var pos =this.body.position;
+    if(this.body.speed<3){
+      imageMode(CENTER);
+      image (this.image,pos.x,pos.y,this.width,this.height)
+    }
+    
+    else{
+      World.remove(world,this.body);
+      push();
+      this.visiblity=this.visiblity-5;
+      tint(255,this.visiblity);
+        image(this.image,pos.x,pos.y,this.width,this.height);
+      pop();
+
+    }
+  }
+};
